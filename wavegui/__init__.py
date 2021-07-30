cfddb9f3c79d967c09418dfb48775b4894351cd5
ruiguang.sun <ruiguang.sun@ihandysoft.com>
2021-07-30 11:07:09 +0800

    support on & handle on

--- wavegui/__init__.py
+++ wavegui/__init__.py
@@ -4,6 +4,7 @@ from .core import Ref, data, pack, Expando
 from .types import *
 from .session import AsyncPage, Query
 from .main import WaveApp
+from .routing import on, handle_on
 
 app = WaveApp()
 Q = Query
